98d2ea2677ac00d3c3c7344978df3ad5421a37c7
Curtis Jewell <swordsman+aptypescript@curtisjewell.name>
2025-04-13 18:58:47 -0600

    build: preparing for 0.1.2 release

---
+++ bin/build-pnpm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd packages/database-cloudflare-d1
+pnpm publish --access public --tag next --no-git-checks
+cd ../../packages/database-mock
+pnpm publish --access public --tag next --no-git-checks
+cd ../../packages/eslint-config
+pnpm publish --access public --tag next --no-git-checks
+cd ../../packages/general
+pnpm publish --access public --tag next --no-git-checks
+cd ../../packages/handlers-response
+pnpm publish --access public --tag next --no-git-checks
+cd ../../packages/localserver-hapi
+pnpm publish --access public --tag next --no-git-checks
+cd ../../packages/types
+pnpm publish --access public --tag next --no-git-checks
+
