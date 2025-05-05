8374cc70fc695befe6b1a2277a2247c6de2fb80f
Curtis Jewell <swordsman+aptypescript@curtisjewell.name>
2025-05-05 08:23:32 -0600

    fix: connect database-node-sqlite to database-mock

--- packages/database-mock/src/router.ts
+++ packages/database-mock/src/router.ts
@@ -1,33 +1,24 @@
 /* SPDX-License-Identifier: MIT
  * SPDX-FileCopyrightText: 2025 Curtis Jewell and other contributors
  */
-import { Database, NotImplementedError } from '@csjewell-activitypub/general'
+import { SQLiteDatabase } from '@csjewell-activitypub/database-node-sqlite'
 import { AnyUsers } from './any-users.ts'
 import { JsonUsers } from './json-users.ts'
 import type { default as Keyv } from 'keyv'
-import type * as AP from '@csjewell-activitypub/types'
+import type { DatabaseSync } from 'node:sqlite'
+import type { Database } from '@csjewell-activitypub/general'
 
-export class DatabaseMock extends Database.SessionRouter implements Database.Router<Keyv> {
+export class DatabaseMock
+  extends SQLiteDatabase
+  implements Database.Router<DatabaseSync> {
   private usersObj : AnyUsers | JsonUsers
 
   constructor(c: Keyv, users?: string) {
-    super(c)
+    super(c, ':memory:')
     this.usersObj = users === undefined ? new AnyUsers() : new JsonUsers(users)
   }
 
-  dbHandle = (): Keyv => { return this.cache }
-  announce = (): Database.StorageHandler<AP.Announce> => { throw new NotImplementedError() }
-  follow = (): Database.StorageHandler<AP.Follow> => { throw new NotImplementedError() }
-  like = (): Database.StorageHandler<AP.Like> => { throw new NotImplementedError() }
-  note = (): Database.StorageHandler<AP.Note> => { throw new NotImplementedError() }
-  actor = (): Database.StorageHandler<AP.Actor> => { throw new NotImplementedError() }
-  documentEntry = (): Database.StorageHandler<AP.CoreObject> => { throw new NotImplementedError() }
-  getDocument = (): Database.DBDocument => { throw new NotImplementedError() }
-  sendToOutbox = (_usernameId: number, _actor: string, _message: AP.CoreObject): AP.OrPromise<boolean> => {
-    throw new NotImplementedError()
-  }
-
-  users = (): AnyUsers | JsonUsers => {
+  override users = (): AnyUsers | JsonUsers => {
     return this.usersObj
   }
 }
--- packages/database-node-sqlite/index.ts
+++ packages/database-node-sqlite/index.ts
@@ -2,4 +2,5 @@
  * SPDX-FileCopyrightText: 2025 Curtis Jewell and other contributors
  */
 
-export * from 'src/database.ts'
+export * from './src/config.ts'
+export * from './src/database.ts'
