74076819e157738351c142955c34c988e589e1c2
Curtis Jewell <swordsman+aptypescript@curtisjewell.name>
2026-07-19 14:56:50 -0600

    build: Finish cleaning up the build process

--- packages/re-pliers/vite.config.ts
+++ packages/re-pliers/vite.config.ts
@@ -1,62 +1,58 @@
 /* SPDX-License-Identifier: MIT
  * SPDX-FileCopyrightText: 2025 Curtis Jewell and other contributors
  */
-import tsTreeshaking from 'rollup-plugin-ts-treeshaking'
 import { visualizer } from 'rollup-plugin-visualizer'
 import upInfo from 'unplugin-info/vite'
-import upIsolatedDecl from 'unplugin-isolated-decl/vite'
 import { viteStaticCopy as staticCopy } from 'vite-plugin-static-copy'
 import { vitePluginVersionMark as versionMark } from 'vite-plugin-version-mark'
 import { defineConfig } from 'vitest/config'
+import { type PluginOption } from 'vite'
 import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins : [
-    preact(),
+    preact() as PluginOption,
     upInfo(),
-    upIsolatedDecl({
-      exclude : 'src/types/*.ts',
-    }),
     staticCopy({
       targets : [{ src: 'src/re-pliers.css', dest: '', }],
     }),
     versionMark({ ifGlobal: false, }),
-    tsTreeshaking(),
-    visualizer(),
+    visualizer({ gzipSize: true, brotliSize: true, emitFile: true, filename: "stats.html", }),
   ],
+
   test : {
     environment : 'jsdom',
   },
+
   build : {
     // lib: { entry: ['src/main.ts'], formats: ['es'], filename: 'dist/re-pliers.module.js', },
-    modulePreload : { polyfill: false, },
-    emptyOutDir   : true,
-    sourcemap     : true,
+    modulePreload        : { polyfill: false, },
+    outDir               : 'dist',
+    minify               : 'oxc',
+    emptyOutDir          : true,
+    sourcemap            : true,
+    license              : true,
+    reportCompressedSize : true,
 
-    // https://rollupjs.org/configuration-options/
-    rollupOptions : {
+    rolldownOptions      : {
       input : {
         're-pliers' : 'src/index.ts',
       },
       output : {
         entryFileNames        : '[name].js',
-        dir                   : 'dist',
         format                : 'esm',
-        compact               : true,
         minifyInternalExports : true,
         sourcemapBaseUrl      : 'https://curtisjewell.dev/',
-        validate              : true,
         generatedCode         : {
-          arrowFunctions  : true,
-          constBindings   : true,
-          objectShorthand : true,
           preset          : 'es2015',
           symbols         : true,
-          // hashCharacters: 'base36',
         },
       },
-      treeshake : 'smallest',
+      treeshake : {
+        moduleSideEffects: false,
+        propertyReadSideEffects: false,
+      },
     },
   },
 })
