766cc5e4a062988c727259bc1be6297a6c46bdd7
Curtis Jewell <swordsman+aptypescript@curtisjewell.name>
2026-07-26 00:01:21 -0600

    monorepo: Fixing git hooks and a few other things
    Commit-Type: build

--- packages/commit-config/index.ts
+++ packages/commit-config/index.ts
@@ -1,4 +1,6 @@
 /* SPDX-License-Identifier: MIT
- * SPDX-FileCopyrightText: 2025 Curtis Jewell and other contributors
+ * SPDX-FileCopyrightText: 2026 Curtis Jewell and other contributors
  */
+export * as Changesets from './src/changeset.ts'
 export * from './src/commitlint.ts'
+
